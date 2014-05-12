5f18dcfabb3ec60ba9940ad459e9ff78bc2560f1
Lubomir Rintel <lkundrak@v3.sk>
2014-05-13 00:35:30 +0200

    Add copyright information

--- lib/Database/DumpTruck.pm
+++ lib/Database/DumpTruck.pm
@@ -564,6 +564,8 @@ heavily inspired by.
 
 =head1 COPYRIGHT
 
+Copyright 2014, Lubomir Rintel
+
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
